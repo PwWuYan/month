012b28e27fd149d25979a0491e0555ec2e81db02
wuyan <1319775253@qq.com>
2019-08-31 10:21:02 +0800

    排版

--- month_A/app/src/views/classify/index.vue
+++ month_A/app/src/views/classify/index.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-      分类
+  <div class="classify">
+    <div class="top">
+      <span v-for="(item,index) in list" :key={index}>{{item}}</span>
+    </div>
+      
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list:['热门','新书','免费','完本']
+    }
+  },
 }
 </script>
 
-<style>
-
+<style lang='scss' scoped>
+.classify{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .top{
+    width: 100%;
+    height: 50px;
+    display: flex;
+   span{
+     flex: 1;
+     text-align: center;
+     line-height: 50px;
+   }
+  }
+}
 </style>
